586985108a5cedaeb9d8d90f8fede03b8038479f
lliorlio2301 <jorge2301@outlook.es>
2026-01-06 19:11:46 +0100

    Speicherung nochmal

--- src/main/java/com/thymeleaf1/lazyvim1/controller/MenueController.java
+++ src/main/java/com/thymeleaf1/lazyvim1/controller/MenueController.java
@@ -1,6 +1,7 @@
 package com.thymeleaf1.lazyvim1.controller;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MenueController {
   private static List<String> jorgeiros = List.of("jorge", "osorio", "salazar");
 
-  @GetMapping
+  @GetMapping("/Hauptfenster")
   public String menueFenster(Model model) {
-    model.addAttribute("lahoradelJorge", LocalDateTime.now());
+    model.addAttribute("lahoradelJorge", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
     model.addAttribute("listevonjorge", jorgeiros);
     return "menueFenster";
   }
--- src/main/java/com/thymeleaf1/lazyvim1/model/Text.java
+++ src/main/java/com/thymeleaf1/lazyvim1/model/Text.java
@@ -1,4 +1,25 @@
 package com.thymeleaf1.lazyvim1.model;
 
-public record Text(String text) {
+public class Text {
+  private String nachricht;
+
+  public Text() {
+  }
+
+  public Text(String nachricht) {
+    this.nachricht = nachricht;
+  }
+
+  @Override
+  public String toString() {
+    return "Text [nachricht=" + nachricht + "]";
+  }
+
+  public void setNachricht(String nachricht) {
+    this.nachricht = nachricht;
+  }
+
+  public String getNachricht() {
+    return this.nachricht;
+  }
 }
